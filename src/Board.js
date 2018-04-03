c6a7abc4c3805e4881b24db9706b852a6c3b8932
chunwing_lam <chunwing_lam@timeinc.com>
2018-04-03 11:39:16 -0400

    added drag and drop, move note state to parent

--- src/Board.js
+++ src/Board.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react';
-import Note from './Note';
-import FaPlus from 'react-icons/lib/fa/plus';
+import React, { Component } from 'react'
+import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
+import HTML5Backend from 'react-dnd-html5-backend'
+import { DragDropContext, DropTarget } from 'react-dnd'
+import { ItemTypes } from './Constants'
+
+const boxTarget = {
+  drop(props, monitor, component) {
+		const item = monitor.getItem()
+		const delta = monitor.getDifferenceFromInitialOffset()
+		const right = Math.round(item.right - delta.x)
+		const top = Math.round(item.top + delta.y)
+
+    component.updatePosition(right, top, item.id);
+	},
+}
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget()
+  }
+}
 
 class Board extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       notes : []
     }
-    this.eachNote = this.eachNote.bind(this);
-    this.update = this.update.bind(this);
-    this.remove = this.remove.bind(this);
-    this.add = this.add.bind(this);
-    this.nextId = this.nextId.bind(this);
+    this.eachNote = this.eachNote.bind(this)
+    this.update = this.update.bind(this)
+    this.remove = this.remove.bind(this)
+    this.add = this.add.bind(this)
+    this.nextId = this.nextId.bind(this)
   }
 
   componentWillMount() {
-    var self = this;
+    var self = this
     if (this.props.count) {
       fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
         .then(response => response.json())
@@ -32,10 +52,17 @@ class Board extends Component {
         ...prevState.notes,
         {
           id: this.nextId(),
-          note: text
+          note: text,
+          right: this.randomBetween(0, window.innerWidth - 150),
+          top: this.randomBetween(0, window.innerHeight - 150),
+          transform: `rotate(${this.randomBetween(-25, 25)})`
         }
       ]
     }))
+  }
+
+  randomBetween(x, y) {
+    return x + Math.ceil(Math.random() * (y-x))
   }
 
   nextId() {
@@ -51,20 +78,30 @@ class Board extends Component {
     }))
   }
 
+  updatePosition(newRight, newTop, i) {
+    this.setState(prevState => ({
+      notes: prevState.notes.map(
+        note => (note.id !== i)? note : {...note, right: newRight, top: newTop}
+      )
+    }))
+  }
+
   remove(id) {
     this.setState(prevState => ({
       notes: prevState.notes.filter(note => note.id !== id)
     }))
   }
+
   eachNote(note, i) {
     return (
-      <Note key={note.id} index={note.id} onChange={this.update} onRemove={this.remove}>
-        {note.note}
+      <Note key={note.id} index={note.id} onChange={this.update} onRemove={this.remove}
+            note={note}>
       </Note>
     )
   }
   render() {
-    return (
+    const { connectDropTarget } = this.props
+    return connectDropTarget(
       <div className="board">
         {this.state.notes.map(this.eachNote)}
         <button onClick={this.add.bind(null, "New Note")}
@@ -76,4 +113,4 @@ class Board extends Component {
   }
 }
 
-export default Board
+export default DragDropContext(HTML5Backend)(DropTarget(ItemTypes.NOTE, boxTarget, collect)(Board))
